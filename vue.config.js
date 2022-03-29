f7251b3a3fd3e5a2ba4bb6cf816f882a62eb9e8c
Roely Chur <gerson.roely@gmail.com>
2022-03-29 08:20:07 -0600

    production config

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,10 @@ module.exports = {
 	devServer: {
 		host: 'localhost'
 	},
+	publicPath: process.env.NODE_ENV === 'production' ? '/apps/dr-movil-app/' : '/',
+	pwa: {
+		workboxOptions: {
+			skipWaiting: true
+		}
+	}
 }
